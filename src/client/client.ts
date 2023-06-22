7a5f5015496566a7216de904d29a772cb3edc00f
Haytham Adel <haytham.adel@mts-egy.com>
2023-06-22 16:29:51 +0300

    playing around with the GUI and folders

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Stats from 'three/examples/jsm/libs/stats.module';
+import { GUI } from 'dat.gui'
 
 const scene = new THREE.Scene()
 scene.background = new THREE.Color(0x448800);
@@ -37,12 +38,24 @@ function onWindowResize() {
 }
 const stats = new Stats();
 document.body.appendChild(stats.dom);
+const gui = new GUI();
+const cubeFolder = gui.addFolder('Cube')
+
+cubeFolder.add(cube.rotation, 'x', 0, Math.PI*2);
+cubeFolder.add(cube.rotation, 'y', 0, Math.PI*2);
+cubeFolder.add(cube.rotation, 'z', 0, Math.PI*2);
+
+const cameraFolder = gui.addFolder('Camera')
+cameraFolder.add(camera.position, 'x', 0, 10)
+cameraFolder.add(camera.position, 'y', 0, 10)
+cameraFolder.add(camera.position, 'z', 0, 10)
+cameraFolder.open()
 
 function animate() {
     requestAnimationFrame(animate)
 
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
+    // cube.rotation.x += 0.01
+    // cube.rotation.y += 0.01
 
     render()
 
